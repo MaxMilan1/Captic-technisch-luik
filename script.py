c1816ebbe168d887b2c9ce933f78a2517791da3a
Max Milan <max.milan@student.hogent.be>
2023-10-31 16:32:31 +0100

    change to script since filename used as "external_file"

--- script.py
+++ script.py
@@ -6,12 +6,12 @@ def extractImageData(inputimage, outputjson):
     image = cv2.imread(inputimage)
 
     if image is not None:
-        filename = os.path.basename(inputimage)
+        # filename = os.path.basename(inputimage) not needed since file provided to docker container as external file all files will be named "external_file"
         height, width = image.shape[:2]
         channels = image.shape[2]
 
         metadata = {
-            "filename": filename,
+            # "filename": filename,
             "height": height,
             "width": width,
             "channels": channels
